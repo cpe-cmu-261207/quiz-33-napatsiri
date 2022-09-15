bf1019ae6fdf676c15fb266d32189fde1663b0f5
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2022-09-15 14:51:58 +0700

    commit

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import LikedCount from "./LikedCount";
 
 export default function Reply(props) {
   return (
-    <div className="d-flex gap-2 my-2 ps-5">
-      <img
-        src={props.profile_img}
-        width="48"
-        height="48"
-        className="rounded-circle"
-        style={{ objectFit: "cover" }}
-      />
-      <div
-        className="rounded rounded-3 p-2"
-        style={{ backgroundColor: "#3A3B3C" }}
-      >
-        <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-          {props.owner}
-        </span>
-        <br />
-        <span style={{ color: "#E4E6EB" }}>{props.content}</span>
-        <likedCount liked={props.liked} />
+    <div>
+      <div className="d-flex gap-2 my-2 ps-5">
+        <img
+          src={props.profileImg}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{props.content}</span>
+          <div
+            className={
+              props.likeNum > 0 ? "d-flex align-items-center gap-1" : "d-none"
+            }
+          >
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>{props.likeNum}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
