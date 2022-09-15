3b0b1b5fc79eba1f310a380e5d77c128bf884830
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2022-09-15 15:15:38 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,28 @@ export default function Home() {
       <div
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
-      ></div>
-      {/* Entire App Container */}
-      <PostOwner
-        liked={99}
-        owner="Napatsiri Phetsri 640612181"
-        content="ขอAได้มั้ยวิชานี้"
-        proFile_img="/proFileImages/IMG_1268.png"
-      />
+      >
+        {/* Entire App Container */}
+        <PostOwner
+          liked={99}
+          owner="Napatsiri Phetsri 640612181"
+          content="ขอAได้มั้ยวิชานี้"
+          proFile_img="/proFileImages/IMG_1268.png"
+        />
 
-      {comments.map((comment, i) => {
-        return (
-          <Comment
-            key={i}
-            liked={comment.likeNum}
-            owner={comment.username}
-            content={comment.commentText}
-            proFile_img={comment.userImagePath}
-            replies={comment.replies}
-          />
-        );
-      })}
+        {comments.map((comment, i) => {
+          return (
+            <Comment
+              key={i}
+              liked={comment.likeNum}
+              owner={comment.username}
+              content={comment.commentText}
+              proFile_img={comment.userImagePath}
+              replies={comment.replies}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
